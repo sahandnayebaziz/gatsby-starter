cf3d01d353601ff82aeccbe767ee3268dcc8aa62
Sahand Nayebaziz <sahand@sahand.me>
2022-03-16 10:31:50 -0700

    update deps

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import { default as React } from "react"
 const Footer = () => {
   return (
     <footer>
-      <div className="">Copyright © 2020 </div>
+      <div className="">Copyright © 2022</div>
     </footer>
   )
 }
